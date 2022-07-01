ece7b1117080c2cbfae568d7345bdbcfd7a8cb79
zubairmalek <zubairmalek95@gmail.com>
2022-07-01 01:56:33 +0100

    completed game, for now..

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+//AUDIO FILES
+const audioFight = new Audio("styles/images/audio/pokemon-fight.mp3");
+const audioOpening = new Audio("styles/images/audio/pokemon-opening.mp3");
+
 class Game {
   constructor() {
     //HARD CODING AS EXAMPLE
     this.selectedPokemon = null;
 
     this.enemies = {
-      name: "charizard",
+      name: "Charizard",
       type: "fire",
       level: 15,
       health: 120,
@@ -35,17 +39,23 @@ class Game {
       // console.log(event.target.className);
       if (event.target.className === "pokemon-screen") {
         // const pickPokemon = document.createElement("div");
+
+        audioOpening.play();
         const loadingScreen = document.querySelector("#loading-screen");
+        const pokemonLoading = document.querySelector("#pokemon-loading-gif");
         const pickPokemon = document.querySelector(".pick-pokemon");
         loadingScreen.style.display = "block";
         setTimeout(() => loadingScreen.remove(), 2000);
-        setTimeout(() => (pickPokemon.style.display = "block"), 2000);
+        setTimeout(() => (pokemonLoading.style.display = "block"), 2000);
+        setTimeout(() => pokemonLoading.remove(), 6000);
+        setTimeout(() => (pickPokemon.style.display = "block"), 6000);
         // loadingScreen.remove();
       }
     });
 
     const selectAllChoices = document.querySelector(".pokemon-choices");
     selectAllChoices.addEventListener("click", (event) => {
+      console.log(event.target.classList);
       if (event.target.classList.contains("charmander")) {
         this.selectedPokemon = this.chooseCharacterCharmander();
         console.log(this.selectedPokemon);
@@ -55,6 +65,9 @@ class Game {
         this.selectedPokemon = this.chooseCharacterBulbasaur();
       }
       this.createFight();
+      const audio = new Audio("styles/images/audio/pokemon-fight.mp3");
+      audioOpening.pause();
+      audioFight.play();
     });
     // console.log(newArray);
   }
@@ -96,7 +109,7 @@ class Game {
           typeOfAttack: "normal",
         },
         {
-          attack: "waterball",
+          attack: "watergun",
           typeOfAttack: "water",
         },
         {
@@ -143,12 +156,12 @@ class Game {
     pokemonSelection.style.display = "none";
 
     const changeImage = document.querySelector("#player-pokemon-img");
-    console.log(changeImage);
-    if (this.selectedPokemon.name === "charmander") {
+    console.log(this.selectedPokemon.name);
+    if (this.selectedPokemon.name === "Charmander") {
       changeImage.setAttribute("src", "./styles/images/charmander.webp");
-    } else if (this.selectedPokemon.name === "squirtle") {
+    } else if (this.selectedPokemon.name === "Squirtle") {
       changeImage.setAttribute("src", "./styles/images/squirtle.gif");
-    } else if (this.selectedPokemon.name === "bulbasaur") {
+    } else if (this.selectedPokemon.name === "Bulbasaur") {
       changeImage.setAttribute("src", "./styles/images/bulbasaur.webp");
     }
 
@@ -156,7 +169,7 @@ class Game {
     changeHTMLNameInBar.innerHTML = this.selectedPokemon.name;
 
     const changeHTMLLevelInBar = document.querySelector(".player-info h3");
-    changeHTMLLevelInBar.innerHTML = "Lvl " + this.selectedPokemon.level;
+    changeHTMLLevelInBar.innerHTML = "Lvl" + this.selectedPokemon.level;
 
     const changeMenuHTMLName = document.querySelector(".left-menu p");
     changeMenuHTMLName.innerHTML =
@@ -164,6 +177,21 @@ class Game {
     const fightScreen = document.querySelector(".fight-screen");
     if (this.selectedPokemon.name) fightScreen.style.display = "block";
 
+    // DISPLAY ENEMY
+
+    const changeEnemyName = document.querySelector(".enemy-info h1");
+    changeEnemyName.innerHTML = this.enemies.name;
+
+    const changeEnemyLevel = document.querySelector(".enemy-info h3");
+    changeEnemyLevel.innerHTML = "Lvl" + this.enemies.level;
+
+    const changeEnemyImage = document.querySelector(".selected-enemy-img img");
+    changeEnemyImage.setAttribute("src", "./styles/images/charizard.gif");
+    changeEnemyImage.style.width = "90px";
+    const changeEnemyImagePosition = document.querySelector(
+      ".selected-enemy-img"
+    );
+    changeEnemyImagePosition.style.left = "304px";
     const displayMenu = document.querySelector(".menu");
     displayMenu.style.display = "flex";
 
@@ -183,6 +211,8 @@ class Fight extends Game {
     selectionOptions.addEventListener("click", (event) => {
       if (event.target.classList.contains("fight-button")) {
         this.pickOptionFight();
+      } else if (event.target.classList.contains("run")) {
+        this.gameLost();
       }
     });
   }
@@ -221,48 +251,94 @@ class Fight extends Game {
     }
   }
 
+  hoverAttack() {
+    const showTypeOfAttack = document.querySelector(".type-of-attack p");
+    const attackOneSelected = document.querySelector(".attack1");
+    const attackTwoSelected = document.querySelector(".attack2");
+    const attackThreeSelected = document.querySelector(".attack3");
+    const goBackSelected = document.querySelector(".go-back");
+    console.log("player attack type is " + this.playerChosenAttack);
+
+    attackOneSelected.addEventListener("mouseenter", (event) => {
+      console.log(event.target.children[0].outerText);
+      showTypeOfAttack.innerHTML = "Type: <br> Normal";
+    });
+
+    attackTwoSelected.addEventListener("mouseenter", (event) => {
+      console.log(event.target.children[0].outerText);
+      if (event.target.children[0].outerText === "EMBER") {
+        showTypeOfAttack.innerHTML = "Type: <br> Fire";
+      } else if (event.target.children[0].outerText === "WATERGUN") {
+        showTypeOfAttack.innerHTML = "Type: <br> Water";
+      } else if (event.target.children[0].outerText === "SEEDBOMB") {
+        showTypeOfAttack.innerHTML = "Type: <br> Grass";
+      }
+    });
+
+    attackThreeSelected.addEventListener("mouseenter", () => {
+      showTypeOfAttack.innerHTML = "Type: <br> Normal";
+    });
+
+    goBackSelected.addEventListener("mouseenter", () => {
+      showTypeOfAttack.innerHTML = "Go back";
+    });
+  }
+
   pickAttack() {
     // HOVER EFFECT
-
+    this.hoverAttack();
     const attackSelected = document.querySelector(".select-attack");
     const enemyHealthBar = document.querySelector("#enemy-health-bar progress");
     const playerHealthBar = document.querySelector(
       "#player-health-bar progress"
     );
+    const attackCommentary = document.querySelector(".attack-commentary");
+    const hideAttackMenu = document.querySelector(".attack-menu");
     console.log(enemyHealthBar);
+    // this.gameOver();
     attackSelected.addEventListener("click", (event) => {
       if (event.target.classList.contains("attack-one")) {
         // console.log(this.selectedPokemon.attacks[0]);
+        // this.gameOver();
         this.playerChosenAttack = this.selectedPokemon.attacks[0];
         this.attack();
         enemyHealthBar.setAttribute("value", this.enemies.health);
-        setTimeout(() => this.defend(), 2000);
+        setTimeout(() => this.defend(), 3000);
         setTimeout(
           () =>
             playerHealthBar.setAttribute("value", this.selectedPokemon.health),
-          2000
+          3000
         );
+        setTimeout(() => (attackCommentary.style.display = "none"), 6000);
+        setTimeout(() => (hideAttackMenu.style.display = "flex"), 6000);
+        // attackCommentary.style.display = "none";
+        // hideAttackMenu.style.display = "flex";
+
         // console.log(this.playerChosenAttack);
       } else if (event.target.classList.contains("attack-two")) {
         this.playerChosenAttack = this.selectedPokemon.attacks[1];
         this.attack();
         enemyHealthBar.setAttribute("value", this.enemies.health);
-        setTimeout(() => this.defend(), 2000);
+        setTimeout(() => this.defend(), 3000);
         setTimeout(
           () =>
             playerHealthBar.setAttribute("value", this.selectedPokemon.health),
-          2000
+          3000
         );
+        setTimeout(() => (attackCommentary.style.display = "none"), 6000);
+        setTimeout(() => (hideAttackMenu.style.display = "flex"), 6000);
       } else if (event.target.classList.contains("attack-three")) {
         this.playerChosenAttack = this.selectedPokemon.attacks[2];
         this.attack();
         enemyHealthBar.setAttribute("value", this.enemies.health);
-        setTimeout(() => this.defend(), 2000);
+        setTimeout(() => this.defend(), 3000);
         setTimeout(
           () =>
             playerHealthBar.setAttribute("value", this.selectedPokemon.health),
-          2000
+          3000
         );
+        setTimeout(() => (attackCommentary.style.display = "none"), 6000);
+        setTimeout(() => (hideAttackMenu.style.display = "flex"), 6000);
       } else if (event.target.classList.contains("back")) {
         this.displayMenu();
       }
@@ -279,62 +355,238 @@ class Fight extends Game {
   }
 
   attack() {
-    if (this.checkTypeOfAttack() === "fire") {
-      const damage = this.checkDamageForFireAttack(this.checkEnemyType());
-      console.log("damage given to enemy is " + damage);
+    const attackCommentaryDiv = document.querySelector(".attack-commentary");
+    const hideAttackMenu = document.querySelector(".attack-menu");
+    const playerAttackCommentary = document.querySelector(
+      ".attack-commentary p"
+    );
+
+    const playerAttackType = this.checkTypeOfAttack();
+    const enemyType = this.checkEnemyType();
+    const healthNumber = document.querySelector(".enemy-health-display p");
+    if (playerAttackType === "fire") {
+      const damage = this.checkDamageForFireAttack(enemyType);
       const enemyHealth = this.enemies.health;
       const damageEnemy = enemyHealth - damage;
       this.enemies.health = damageEnemy;
-    } else if (this.checkTypeOfAttack() === "normal") {
+      if (this.enemies.health > 0) {
+        healthNumber.innerHTML = this.enemies.health + "/120";
+      } else {
+        healthNumber.innerHTML = "0/120";
+      }
+      // SHOW ATTACK COMMENTARY
+      attackCommentaryDiv.style.display = "flex";
+      attackCommentaryDiv.style.backgroundColor = "white";
+      attackCommentaryDiv.style.color = "black";
+      hideAttackMenu.style.display = "none";
+      playerAttackCommentary.innerHTML =
+        this.selectedPokemon.name +
+        " uses " +
+        this.playerChosenAttack.attack +
+        " makes damage of " +
+        damage +
+        "hp";
+
+      // CHECK IF GAME IS OVER
+      if (this.enemies.health <= 0) {
+        // this.gameWon();
+        setTimeout(() => this.gameWon(), 2000);
+      }
+    } else if (playerAttackType === "normal") {
       const damage = this.checkDamageForNormalAttack();
       const enemyHealth = this.enemies.health;
       const damageEnemy = enemyHealth - damage;
       this.enemies.health = damageEnemy;
-    } else if (this.checkTypeOfAttack() === "water") {
-      const damage = this.checkDamageForWaterAttack(this.checkEnemyType());
-
+      if (this.enemies.health > 0) {
+        healthNumber.innerHTML = this.enemies.health + "/120";
+      } else {
+        healthNumber.innerHTML = "0/120";
+      }
+      attackCommentaryDiv.style.display = "flex";
+      attackCommentaryDiv.style.backgroundColor = "white";
+      attackCommentaryDiv.style.color = "black";
+      hideAttackMenu.style.display = "none";
+      playerAttackCommentary.innerHTML =
+        this.selectedPokemon.name +
+        " uses " +
+        this.playerChosenAttack.attack +
+        " makes damage of " +
+        damage +
+        "hp";
+      if (this.enemies.health <= 0) {
+        setTimeout(() => this.gameWon(), 2000);
+      }
+      console.log("enemy damage is now " + this.enemies.health);
+    } else if (playerAttackType === "water") {
+      const damage = this.checkDamageForWaterAttack(enemyType);
       const enemyHealth = this.enemies.health;
       const damageEnemy = enemyHealth - damage;
       this.enemies.health = damageEnemy;
-      return this.enemies.health;
-    } else if (this.checkTypeOfAttack() === "grass") {
-      const damage = this.checkDamageForGrassAttack(this.checkEnemyType());
-
+      if (this.enemies.health > 0) {
+        healthNumber.innerHTML = this.enemies.health + "/120";
+      } else {
+        healthNumber.innerHTML = "0/120";
+      }
+      attackCommentaryDiv.style.display = "flex";
+      attackCommentaryDiv.style.backgroundColor = "white";
+      attackCommentaryDiv.style.color = "black";
+      hideAttackMenu.style.display = "none";
+      playerAttackCommentary.innerHTML =
+        this.selectedPokemon.name +
+        " uses " +
+        this.playerChosenAttack.attack +
+        " makes damage of " +
+        damage +
+        "hp";
+      if (this.enemies.health <= 0) {
+        setTimeout(() => this.gameWon(), 2000);
+      }
+      console.log("enemy damage is now " + this.enemies.health);
+    } else if (playerAttackType === "grass") {
+      const damage = this.checkDamageForGrassAttack(enemyType);
       const enemyHealth = this.enemies.health;
       const damageEnemy = enemyHealth - damage;
       this.enemies.health = damageEnemy;
-      return this.enemies.health;
+      if (this.enemies.health > 0) {
+        healthNumber.innerHTML = this.enemies.health + "/120";
+      } else {
+        healthNumber.innerHTML = "0/120";
+      }
+      attackCommentaryDiv.style.display = "flex";
+      attackCommentaryDiv.style.backgroundColor = "white";
+      attackCommentaryDiv.style.color = "black";
+      hideAttackMenu.style.display = "none";
+      playerAttackCommentary.innerHTML =
+        this.selectedPokemon.name +
+        " uses " +
+        this.playerChosenAttack.attack +
+        " makes damage of " +
+        damage +
+        "hp";
+      if (this.enemies.health <= 0) {
+        setTimeout(() => this.gameWon(), 2000);
+      }
+      console.log("enemy damage is now " + this.enemies.health);
     }
   }
   defend() {
-    this.enemyChosenAttack = this.pickEnemyAttack();
-    console.log("were inside defend....." + this.enemyChosenAttack);
-    if (this.enemyChosenAttack === "fire") {
-      const damage = this.checkDamageForFireAttack(this.checkPlayerType());
+    const attackCommentaryDiv = document.querySelector(".attack-commentary");
+    const hideAttackMenu = document.querySelector(".attack-menu");
+    const enemyAttackCommentary = document.querySelector(
+      ".attack-commentary p"
+    );
+
+    // console.log("were inside defend");
+    const enemyAttackType = this.checkEnemyAttack();
+    const playerType = this.checkPlayerType();
+    const healthNumber = document.querySelector(".health-display p");
+    // console.log(enemyAttackType);
+    // console.log("were inside defend....." + this.enemyChosenAttack);
+    if (enemyAttackType === "fire") {
+      const damage = this.checkDamageForFireAttack(playerType);
       console.log("damage i take is " + damage);
       const playerHealth = this.selectedPokemon.health;
       const damagePlayer = playerHealth - damage;
       this.selectedPokemon.health = damagePlayer;
-    } else if (this.enemyChosenAttack === "normal") {
+      if (this.selectedPokemon.health > 0) {
+        healthNumber.innerHTML = this.selectedPokemon.health + "/120";
+      } else {
+        healthNumber.innerHTML = "0/120";
+      }
+      attackCommentaryDiv.style.display = "flex";
+      attackCommentaryDiv.style.backgroundColor = "black";
+      attackCommentaryDiv.style.color = "white";
+      hideAttackMenu.style.display = "none";
+      enemyAttackCommentary.innerHTML =
+        this.enemies.name +
+        " uses " +
+        this.enemyChosenAttack.attack +
+        " makes damage of " +
+        damage +
+        "hp";
+
+      if (this.selectedPokemon.health <= 0) {
+        setTimeout(() => this.gameLost(), 2000);
+      }
+      console.log("my damage is now " + this.selectedPokemon.health);
+    } else if (enemyAttackType === "normal") {
       const damage = this.checkDamageForNormalAttack();
       console.log("damage i take is " + damage);
       const playerHealth = this.selectedPokemon.health;
       const damagePlayer = playerHealth - damage;
       this.selectedPokemon.health = damagePlayer;
-    } else if (this.enemyChosenAttack === "water") {
-      const damage = this.checkDamageForWaterAttack(this.checkPlayerType());
+      if (this.selectedPokemon.health > 0) {
+        healthNumber.innerHTML = this.selectedPokemon.health + "/120";
+      } else {
+        healthNumber.innerHTML = "0/120";
+      }
 
+      attackCommentaryDiv.style.display = "flex";
+      attackCommentaryDiv.style.backgroundColor = "black";
+      attackCommentaryDiv.style.color = "white";
+      hideAttackMenu.style.display = "none";
+      enemyAttackCommentary.innerHTML =
+        this.enemies.name +
+        " uses " +
+        this.enemyChosenAttack.attack +
+        " makes damage of " +
+        damage +
+        "hp";
+
+      if (this.selectedPokemon.health <= 0) {
+        setTimeout(() => this.gameLost(), 2000);
+      }
+      console.log("my damage is now " + this.selectedPokemon.health);
+    } else if (enemyAttackType === "water") {
+      const damage = this.checkDamageForWaterAttack(playerType);
       const playerHealth = this.selectedPokemon.health;
       const damagePlayer = playerHealth - damage;
       this.selectedPokemon.health = damagePlayer;
-      return this.selectedPokemon.health;
-    } else if (this.enemyChosenAttack === "grass") {
-      const damage = this.checkDamageForGrassAttack(this.checkPlayerType());
+      healthNumber.innerHTML = this.selectedPokemon.health + "/120";
 
+      attackCommentaryDiv.style.display = "flex";
+      attackCommentaryDiv.style.backgroundColor = "black";
+      attackCommentaryDiv.style.color = "white";
+      hideAttackMenu.style.display = "none";
+      enemyAttackCommentary.innerHTML =
+        this.enemies.name +
+        " uses " +
+        this.enemyChosenAttack.attack +
+        " makes damage of " +
+        damage +
+        "hp";
+
+      if (this.selectedPokemon.health <= 0) {
+        setTimeout(() => this.gameLost(), 2000);
+      }
+      console.log("my damage is now " + this.selectedPokemon.health);
+    } else if (enemyAttackType === "grass") {
+      const damage = this.checkDamageForGrassAttack(playerType);
       const playerHealth = this.selectedPokemon.health;
       const damagePlayer = playerHealth - damage;
       this.selectedPokemon.health = damagePlayer;
-      return this.selectedPokemon.health;
+      if (this.selectedPokemon.health > 0) {
+        healthNumber.innerHTML = this.selectedPokemon.health + "/120";
+      } else {
+        healthNumber.innerHTML = "0/120";
+      }
+
+      attackCommentaryDiv.style.display = "flex";
+      attackCommentaryDiv.style.backgroundColor = "black";
+      attackCommentaryDiv.style.color = "white";
+      hideAttackMenu.style.display = "none";
+      enemyAttackCommentary.innerHTML =
+        this.enemies.name +
+        " uses " +
+        this.enemyChosenAttack.attack +
+        " makes damage of " +
+        damage +
+        "hp";
+
+      if (this.selectedPokemon.health <= 0) {
+        setTimeout(() => this.gameLost(), 2000);
+      }
+      console.log("my damage is now " + this.selectedPokemon.health);
     }
   }
 
@@ -412,6 +664,14 @@ class Fight extends Game {
       const attackDamage = Math.round(Math.random() * (70 - 40 + 1) + 40);
       return attackDamage;
     }
+  }
+
+  gameWon() {
+    location.href = "game-won.html";
+  }
+
+  gameLost() {
+    location.href = "game-lost.html";
   }
 }
 
